f94abab260d91b6f79a7691a639730ea47311c9a
Agustin Polo <poloagustin@gmail.com>
2015-05-01 21:40:45 -0300

    mongoose configured, models created

---
+++ models/Comments.js
@@ -0,0 +1,12 @@
+/// <reference path="../typings/mongoose/mongoose.d.ts" />
+
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema({
+	body: String,
+	author: String,
+	upvotes: {type: Number, default: 0},
+	post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'}
+});
+
+mongoose.model('Comment', CommentSchema);
