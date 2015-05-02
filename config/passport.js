4eafa7973a09c23069e2b3ed3aa12a3010e5d675
Agustin Polo <poloagustin@gmail.com>
2015-05-02 13:19:10 -0300

    authentication developed in backend

---
+++ config/passport.js
@@ -0,0 +1,28 @@
+/// <reference path="../typings/passport/passport.d.ts" />
+/// <reference path="../typings/passport-local/passport-local.d.ts" />
+/// <reference path="../typings/mongoose/mongoose.d.ts" />
+
+var passport = 	require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy(
+	function (username, password, done) {
+		User.findOne({username: username}, function (err, user) {
+			if (err) {
+				return done(err);
+			}
+			
+			if (!user) {
+				return done(null, false, {message: 'Incorrect username.'});
+			}
+			
+			if (!user.validPassword(password)) {
+				return done(null, false, {message: 'Incorrect password.'});
+			}
+			
+			return done(null, user);
+		});
+	}
+));
