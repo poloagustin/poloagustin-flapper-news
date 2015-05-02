4b1008925b096259c0bbe2ad38eb1f8c91e1a916
Agustin Polo <poloagustin@gmail.com>
2015-05-02 01:16:08 -0300

    backend developed

--- models/Posts.js
+++ models/Posts.js
@@ -3,10 +3,15 @@
 var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
-	title: String,
+	title: {type: String, default: 'NO TITLE'},
 	link: String,
 	upvotes: {type: Number, default: 0},
 	comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 });
+
+PostSchema.methods.upvote = function (cb) {
+	this.upvotes += 1;
+	this.save(cb);
+};
 
 mongoose.model('Post', PostSchema);
--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,52 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
-/* GET home page. */
+router.param('post', function (req, res, next, id) {
+  var query = Post.findById(id);
+  
+  query.exec(function (err, post) {
+    if (err) {
+      next(err);
+    }
+    
+    if (!post) {
+      return next(new Error('can\'t find post'));
+    }
+    
+    req.post = post;
+    return next();
+  });
+});
+
+router.param('comment', function (req, res, next, id) {
+  var query = Comment.findById(id);
+  
+  query.exec(function (err, comment) {
+    if (err) {
+      next(err);
+    }
+    
+    if (!comment) {
+      return next(new Error('can\'t find comment'));
+    }
+    
+    req.comment = comment;
+    return next();
+  });
+});
+
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/posts/:post', function (req, res, next) {
+    req.post.populate('comments', function (err, post) {
+      if (err) {
+        next(err);
+      }
+      
+      res.json(req.post);
+    })
 });
 
 router.get('/posts', function (req, res, next) {
@@ -24,13 +67,52 @@ router.get('/posts', function (req, res, next) {
 
 router.post('/posts', function (req, res, next) {
   var post =  new Post(req.body);
-  
   post.save(function (err, post) {
     if (err) {
       return next(err);
     }
     
     res.json(post);
+  });
+});
+
+router.put('/posts/:post/upvote', function (req, res, next) {
+  req.post.upvote(function (err, post) {
+    if (err) {
+      next(err);
+    }
+    
+    res.json(post);
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
+  req.comment.upvote(function (err, comment) {
+    if (err) {
+      next(err);
+    }
+    
+    res.json(comment);
+  });
+});
+
+router.post('/posts/:post/comments', function (req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+  
+  comment.save(function (err, comment) {
+    if (err) {
+      next(err);
+    }
+    
+    req.post.comments.push(comment);
+    req.post.save(function (err, post) {
+      if (err) {
+        next(err);
+      }
+      
+      res.json(comment);
+    });
   });
 });
 
